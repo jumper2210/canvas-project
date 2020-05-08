1324fd65048ca875a2f765540c57a5c2ca0f8f9c
jumper2210 <47790273+jumper2210@users.noreply.github.com>
2020-05-08 10:54:30 +0200

    Object SmallCircle: added! fixing bugs

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -12,12 +12,17 @@ const mouse = {
 };
 //variables
 let bigCircles = [];
+let bcLength = bigCircles.length;
+let smallCircles = [];
 let bRadius;
 let xB;
 let yB;
 let dx;
 let dy;
-const friction = 0.69;
+let xs;
+let ys;
+let sRadius;
+const friction = 0.19;
 const gravity = 1;
 // addEventListener("mousemove", (event) => {
 //   mouse.x = event.clientX;
@@ -27,10 +32,32 @@ const gravity = 1;
 addEventListener("resize", () => {
   canvas.width = innerWidth;
   canvas.height = innerHeight;
-  bigCircles.length === 1 ? 0 : init();
+  bcLength === 1 ? 0 : init();
 });
 // Objects
-class Circle {
+class SmallCircle {
+  constructor(x, y, dx, dy, radius, color) {
+    this.xs = x;
+    this.ys = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
+    this.color = color;
+  }
+  draw() {
+    c.beginPath();
+    c.arc(this.xs, this.ys, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.fill();
+    c.stroke();
+    c.closePath();
+  }
+  update() {
+    this.draw();
+  }
+}
+
+class BigCircle {
   constructor(x, y, dx, dy, radius, color) {
     this.x = x;
     this.y = y;
@@ -49,12 +76,22 @@ class Circle {
   }
   update() {
     if (this.y + this.radius + this.dy > canvas.height) {
-      this.dy = -this.dy * friction;
+      bigCircles = [];
+      if (bcLength === 0) {
+        bigCircles = [];
+        for (let i = 0; i < 3; i++) {
+          sRadius = 15;
+          xs = randomIntFromRange(sRadius, canvas.width - sRadius);
+          ys = 93;
+          dx = 20;
+          dy = 20;
+          smallCircles.push(new SmallCircle(xs, ys, dx, dy, sRadius, "red"));
+        }
+      }
     } else {
-      this.dy += gravity;
+      this.dy += gravity * 0.2;
     }
     this.y += this.dy;
-
     this.draw();
   }
 }
@@ -64,17 +101,20 @@ const init = () => {
     bRadius = 85;
     xB = randomIntFromRange(bRadius, canvas.width - bRadius);
     yB = 93;
-    dx = randomIntFromRange(-2, 2);
+    dx = randomIntFromRange(-1, 1);
     dy = randomIntFromRange(-2, 2);
-    bigCircles.push(new Circle(xB, yB, dx, dy, bRadius, "red"));
+    bigCircles.push(new BigCircle(xB, yB, dx, dy, bRadius, "red"));
   }
 };
 
 const animate = () => {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, canvas.width, canvas.height);
-  bigCircles.forEach((bb) => {
-    bb.update();
+  bigCircles.forEach((bc) => {
+    bc.update();
+  });
+  smallCircles.forEach((sc) => {
+    sc.update();
   });
 };
 init();
